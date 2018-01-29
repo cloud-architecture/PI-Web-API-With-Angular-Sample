49c3e74bd9b79b8edcde313ea4c386f2c89b34c3
Marcos Vainer Loeff <mloeff@osisoft.com>
2018-01-29 12:24:00 -0200

    Upgraded to Angular 5

---
+++ VersionWithClientLibrary5/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { PIWebAPIService } from 'angular-piwebapi';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [PIWebAPIService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+
